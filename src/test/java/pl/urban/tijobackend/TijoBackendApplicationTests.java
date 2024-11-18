8afd82f15d2251e3518e3ff011d3f5763688ae49
Gosqu248 <grzegorzurban248@gmail.com>
2024-11-18 11:55:41 +0100

    first commit

---
+++ src/test/java/pl/urban/tijobackend/TijoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.urban.tijobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TijoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
